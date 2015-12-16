8cf935655ef25d0d57392bd9a55c6fd95b9ea96d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-16 17:55:37 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # hydrate-app
+> Quick app pre-loading using saved HTML snapshot
+
+[![NPM][hydrate-app-icon] ][hydrate-app-url]
+
+[![Build status][hydrate-app-ci-image] ][hydrate-app-ci-url]
+[![semantic-release][semantic-image] ][semantic-url]
+
+### Small print
+
+Author: Gleb Bahmutov &copy; 2015
+
+* [@bahmutov](https://twitter.com/bahmutov)
+* [glebbahmutov.com](http://glebbahmutov.com)
+* [blog](http://glebbahmutov.com/blog/)
+
+License: MIT - do anything with the code, but don't blame me if it does not work.
+
+Spread the word: tweet, star on github, etc.
+
+Support: if you find any problems with this module, email / tweet /
+[open issue](https://github.com/bahmutov/hydrate-app/issues) on Github
+
+## MIT License
+
+Copyright (c) 2015 Gleb Bahmutov
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+[hydrate-app-icon]: https://nodei.co/npm/hydrate-app.png?downloads=true
+[hydrate-app-url]: https://npmjs.org/package/hydrate-app
+[hydrate-app-ci-image]: https://travis-ci.org/bahmutov/hydrate-app.png?branch=master
+[hydrate-app-ci-url]: https://travis-ci.org/bahmutov/hydrate-app
+[semantic-image]: https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg
+[semantic-url]: https://github.com/semantic-release/semantic-release
---
+++ hydrate-app.js
@@ -0,0 +1,190 @@
+'use strict'
+
+function noop () {}
+
+function getBottle (selectId, verbose, verboseUi) {
+  var log = verbose ? console.log.bind(console) : noop
+
+  function formDrySelectorId (id) {
+    return 'dry-' + id
+  }
+
+  var dryId = formDrySelectorId(selectId)
+
+  // TODO factor out into separate module
+  var display = (function initOverlay () {
+    var overlay, message
+
+    function createOverlay () {
+      if (overlay) {
+        return
+      }
+
+      overlay = document.createElement('div')
+      var style = overlay.style
+      style.width = '100%'
+      style.height = '100%'
+      style.opacity = 0.5
+      style.position = 'fixed'
+      style.left = 0
+      style.top = 0
+      style.backgroundColor = 'hsla(187, 100%, 42%, 0.12)'
+      document.body.appendChild(overlay)
+    }
+
+    function createMessage (text) {
+      var style
+      if (!message) {
+        message = document.createElement('h3')
+        style = message.style
+        style.color = '#333'
+        style.position = 'fixed'
+        style.bottom = '0em'
+        style.right = '1em'
+        style.backgroundColor = '#7FFFD4'
+        style.borderRadius = '5px'
+        style.borderWidth = '1px'
+        style.borderColor = '#73E1BC'
+        style.borderStyle = 'solid'
+        style.padding = '1em 2em'
+        document.body.appendChild(message)
+      }
+      message.textContent = text
+    }
+
+    function closeOverlay () {
+      if (overlay) {
+        document.body.removeChild(overlay)
+        overlay = null
+      }
+    }
+
+    function closeMessage () {
+      if (message) {
+        document.body.removeChild(message)
+        message = null
+      }
+    }
+
+    return {
+      message: {
+        show: function show (text) {
+          createMessage(text)
+        },
+        hide: function (timeoutMs) {
+          if (timeoutMs) {
+            setTimeout(closeMessage, timeoutMs)
+          } else {
+            closeMessage()
+          }
+        }
+      },
+      overlay: {
+        show: function show (text) {
+          createOverlay()
+          if (text) {
+            createMessage(text)
+          }
+        },
+        hide: function hide (timeoutMs) {
+          if (timeoutMs) {
+            setTimeout(closeOverlay, timeoutMs)
+          } else {
+            closeOverlay()
+          }
+        }
+      }
+    }
+  }())
+
+  /* global localStorage */
+
+  return {
+    // clears any saved HTML
+    recycle: function recycle () {
+      localStorage.removeItem(selectId)
+      log('removed HTML from localStorage')
+      if (verboseUi) {
+        display.message.show('Cleared storage')
+        display.message.hide(1000)
+      }
+    },
+    // saves HTML snapshot for a given module
+    refill: function refill () {
+      var html = document.getElementById(selectId).outerHTML
+      localStorage.setItem(selectId, html)
+      log('poured', selectId, html)
+      // if (verboseUi) {
+      //   display.message.show('Saved application UI')
+      //   display.message.hide(1000)
+      // }
+    },
+    // takes saved HTML snapshot and creates
+    // a temporary static DOM, allowing real app
+    // to load in hidden mode
+    open: function open () {
+      log('opening', selectId)
+      if (verboseUi) {
+        display.overlay.show('Web application is loading ...')
+      }
+
+      var html = localStorage.getItem(selectId)
+      if (html) {
+        html = html.replace('id="' + selectId + '"',
+          'id="' + dryId + '"')
+        var el = document.getElementById(selectId)
+        el.insertAdjacentHTML('beforebegin', html)
+        el.style.visibility = 'hidden'
+        el.style.display = 'none'
+      }
+    },
+    // when application is ready, replaces the static
+    // DRY content with fully functioning application
+    drink: function drink () {
+      log('drinking', selectId)
+      if (verboseUi) {
+        display.message.show('Web application is running')
+        display.overlay.hide()
+        display.message.hide(1000)
+      }
+
+      var dryEl = document.getElementById(dryId)
+      if (dryEl) {
+        dryEl.parentNode.removeChild(dryEl)
+      }
+      var appEl = document.getElementById(selectId)
+      appEl.style.visibility = ''
+      appEl.style.display = 'initial'
+    }
+  }
+}
+
+!(function initBottle () {
+  function findAttribute (attributes, name) {
+    var found
+    Array.prototype.some.call(attributes, function (attr) {
+      if (attr.name === name) {
+        found = attr
+        return found
+      }
+    })
+    return found && found.value
+  }
+
+  var scripts = document.querySelectorAll('script')
+  var lastScript = scripts[scripts.length - 1]
+
+  var id = findAttribute(lastScript.attributes, 'id') || 'app'
+  var verbose = findAttribute(lastScript.attributes, 'verbose') === 'true'
+  var verboseUi = findAttribute(lastScript.attributes, 'verbose-ui') === 'true'
+  var shouldHydrateName = findAttribute(lastScript.attributes, 'on') || 'hydrate'
+  var shouldHydrate = window[shouldHydrateName]
+
+  var bottle = getBottle(id, verbose, verboseUi)
+  if (!shouldHydrate) {
+    bottle.open = bottle.drink = noop
+  }
+
+  bottle.open()
+  window.bottle = bottle
+}())
