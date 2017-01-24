e11025a761cc8bd99199cf28930318cc54707009
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-23 22:16:29 -0500

    fix(deps): update some dependencies

--- dist/hydrate-app.js
+++ dist/hydrate-app.js
@@ -196,25 +196,58 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 1 */
 /***/ function(module, exports) {
 
+	'use strict'
+
 	var tinyToast
+
+	function createCssStyleSheet () {
+	  // code taken from
+	  // https://davidwalsh.name/add-rules-stylesheets
+	  const style = document.createElement('style')
+	  // WebKit hack :(
+	  // style.appendChild(document.createTextNode(''))
+	  document.head.appendChild(style)
+	  return style.sheet
+	}
+
+	function insertStyle (className, sheet, style) {
+	  // insert a rule for each style property
+	  Object.keys(style).forEach((key) => {
+	    const value = style[key]
+	    sheet.insertRule(`.${className} { ${key}: ${value} }`, 0)
+	  })
+	}
+
+	function createTinyToastStyle () {
+	  const className = 'tinyToast'
+	  const style = {
+	    color: '#333',
+	    position: 'fixed',
+	    bottom: '0em',
+	    right: '1em',
+	    'background-color': '#7FFFD4',
+	    'border-radius': '5px',
+	    'border-width': '1px',
+	    'border-color': '#73E1BC',
+	    'border-style': 'solid',
+	    padding: '1em 2em',
+	    'z-index': 1000
+	  }
+
+	  const sheet = createCssStyleSheet()
+	  insertStyle(className, sheet, style)
+	  return className
+	}
 
 	function createDom () {
 	  if (tinyToast) {
 	    return tinyToast
 	  }
 
+	  const className = createTinyToastStyle()
+
 	  tinyToast = document.createElement('h3')
-	  var style = tinyToast.style
-	  style.color = '#333'
-	  style.position = 'fixed'
-	  style.bottom = '0em'
-	  style.right = '1em'
-	  style.backgroundColor = '#7FFFD4'
-	  style.borderRadius = '5px'
-	  style.borderWidth = '1px'
-	  style.borderColor = '#73E1BC'
-	  style.borderStyle = 'solid'
-	  style.padding = '1em 2em'
+	  tinyToast.classList.add(className)
 	  document.body.appendChild(tinyToast)
 	  return tinyToast
 	}
@@ -238,13 +271,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }
 	}
 
+	function hide (timeoutMs) {
+	  maybeDefer(closeMessage, timeoutMs)
+	}
+
 	var tinyToastApi = {
 	  show: function show (text) {
 	    createMessage(text)
+	    return tinyToastApi
 	  },
-	  hide: function (timeoutMs) {
-	    maybeDefer(closeMessage, timeoutMs)
-	  }
+	  hide: hide
 	}
 
 	module.exports = tinyToastApi
